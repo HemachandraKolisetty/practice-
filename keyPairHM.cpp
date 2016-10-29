df75001ffd86617ac673d1cdb46d3dfc0bd2efae
HemachandraKolisetty <kolisetty.hemachandra510@gmail.com>
2016-10-29 15:33:49 +0530

    Add files via upload

---
+++ keyPairHM.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+bool findPair(int a[],int n,int x){
+	bool binMap[1000]={0};
+	int temp;
+	for(int i=0;i<n;i++){
+		temp = x-a[i];
+		if(temp>=0) {
+			if(binMap[temp]==1) return true;
+		}
+		binMap[a[i]]=1;
+	}
+	return false;
+}
+
+int main(){
+	int t;
+	cin>>t;
+	for(int j=0;j<t;j++){
+		int n,x;
+		cin>>n>>x;
+		int a[n];
+		for(int i=0;i<n;i++) cin>>a[i];
+		if(findPair(a,n,x)) cout<<"Yes"<<endl;
+		else cout<<"No"<<endl;
+	}
+}
